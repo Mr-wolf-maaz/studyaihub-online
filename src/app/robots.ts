a1f984de4a207d0bd6a5652092bd72b39391847a
Mazhar ali shah <saymaaz232@gmail.com>
2026-07-21 11:01:19 +0500

    Fix domain: change studyaihub.com to studyaihub.online

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -3,6 +3,6 @@ import { MetadataRoute } from "next";
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: { userAgent: "*", allow: "/", disallow: "/admin/" },
-    sitemap: "https://studyaihub.com/sitemap.xml",
+    sitemap: "https://studyaihub.online/sitemap.xml",
   };
 }
